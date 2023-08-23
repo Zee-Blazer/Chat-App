7ccff9c0ce956d76fd5064231c09e05d76cf50e6
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2023-08-23 21:58:30 +0100

    Added the new changes

--- src/Services/Axios/axios-api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: "http://10.0.2.2:3004/"
-})
-
-export const uriLink = "http://10.0.2.2:3004/"
-export const picUriLink = "http://10.0.2.2:3004/profile/pic"
