eb8b300db9f754c31d2c20626e2f880c75460430
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-10-06 17:59:40 +0100

    Everything in order

--- app/src/Services/Axios/axios-api.js
+++ app/src/Services/Axios/axios-api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 export default axios.create({
-    // baseURL: "http://localhost:3004/"
+    baseURL: "http://localhost:3004/"
     // baseURL: "https://cee2-197-210-52-68.ngrok-free.app"
     // baseURL: "https://chatapp-backend-api.onrender.com/"
-    baseURL: "http://10.0.2.2:3004/"
+    // baseURL: "http://10.0.2.2:3004/"
 })
 
 // export const uriLink = "http://10.0.2.2:3004/"
